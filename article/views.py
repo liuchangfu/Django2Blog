457025dd8309566901dd0e9abe155aaaa5026005
Liuchangfu <shift_1220@163.com>
2019-09-16 22:34:33 +0800

    增加分页功能

--- article/views.py
+++ article/views.py
@@ -5,12 +5,19 @@ import markdown
 from article.forms import ArticlePostForm
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 
 
 # Create your views here.
 def article_list(request):
     # 取出所有博客文章
-    articles = ArticlePost.objects.all()
+    article_list = ArticlePost.objects.all()
+    # 每页显示5篇文章
+    paginator = Paginator(article_list, 1)
+    # 获取 url 中的页码
+    page = request.GET.get('page')
+    # 将导航对象相应的页码内容返回给 articles
+    articles = paginator.get_page(page)
     return render(request, 'article/list.html', locals())
 
 
