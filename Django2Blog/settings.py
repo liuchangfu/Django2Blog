d5e5d147915f6bf11203c3a3bde8b35255b85515
liuchangfu <shift_1220@163.com>
2019-10-15 08:50:11 +0800

    重新生成数据库

--- Django2Blog/settings.py
+++ Django2Blog/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'taggit',
     'imagekit',
     'ckeditor',
+    'mptt',
 ]
 
 MIDDLEWARE = [
--- comment/models.py
+++ comment/models.py
@@ -2,19 +2,29 @@ from django.db import models
 from django.contrib.auth.models import User
 from article.models import ArticlePost
 from ckeditor.fields import RichTextField
+from mptt.models import MPTTModel, TreeForeignKey
 
 
 # Create your models here.
 
-class Comment(models.Model):
+class Comment(MPTTModel):
     article = models.ForeignKey(ArticlePost, on_delete=models.CASCADE, related_name='comments', verbose_name='评论文章')
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments', verbose_name='评论用户')
     # body = models.TextField(verbose_name='评论内容')
     body = RichTextField(verbose_name='评论内容')
     created = models.DateTimeField(auto_now_add=True, verbose_name='评论时间')
+    parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children',
+                            verbose_name='父评论')
+    reply_to = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE, related_name='replyers',
+                                 verbose_name='回复')
 
-    class Meta:
-        ordering = ['-created']
+    # class Meta:
+    #     ordering = ['-created']
+    #     verbose_name_plural = '评论管理'
+    #     verbose_name = '评论管理'
+
+    class MPTTMeta:
+        order_insertion_by = ['created']
         verbose_name_plural = '评论管理'
         verbose_name = '评论管理'
 
