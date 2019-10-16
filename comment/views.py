6062155843dfa4bcca785a33172c49575b597421
liuchangfu <shift_1220@163.com>
2019-10-16 09:06:08 +0800

    增加评论功能

--- comment/views.py
+++ comment/views.py
@@ -3,12 +3,13 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from article.models import ArticlePost
 from comment.forms import CommentForm
+from .models import Comment
 
 
 # Create your views here.
 
 @login_required(login_url='/user_profile/login/')
-def post_comment(request, article_id):
+def post_comment(request, article_id, parent_comment_id=None):
     article = get_object_or_404(ArticlePost, id=article_id)
     if request.method == "POST":
         comment_form = CommentForm(request.POST)
@@ -16,9 +17,21 @@ def post_comment(request, article_id):
             new_comment = comment_form.save(commit=False)
             new_comment.article = article
             new_comment.user = request.user
+            # 二级回复
+            if parent_comment_id:
+                parent_comment = Comment.objects.get(id=parent_comment_id)
+                # 若回复层级超过二级，则转换为二级
+                new_comment.parent_id = parent_comment.get_root().id
+                # 被回复人
+                new_comment.reply_to = parent_comment.user
+                new_comment.save()
+                return HttpResponse('200 OK')
             new_comment.save()
             return redirect(article)
         else:
             return HttpResponse('表单内容有误，请重新输入！！')
+    elif request.method == 'GET':
+        comment_form = CommentForm()
+        return render(request, 'comment/reply.html', locals())
     else:
         return HttpResponse('发表评论仅接受POST请求。')
